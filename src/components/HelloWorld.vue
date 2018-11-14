9301a76e867de0920ab2e117a33ecb4b5cb50c44
adam <adam.jacewicz4@gmail.com>
2018-11-14 13:37:18 +0100

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>{{ msg }}</strong>!
+      </p>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
