a67f901d9e948d367c47c75d109c6e3b20ce30ca
r.krukowski <rafal.krukowski@solsoft.pl>
2019-07-20 16:01:25 +0200

    Create dev branch

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -18,5 +18,10 @@ export default new Router({
       name: 'about',
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+    {
+      path: '/login',
+      name: 'Login',
+      component: () => import('./views/login/login.views.vue'),
+    },
   ],
 });
